d916e9909f47285d320838b443d2fd81733c851e
dvyakymenko <dvyakymenko@gmail.com>
2025-07-08 20:56:29 +0300

    second commit

---
+++ Lesson2/Lesson2.py
@@ -0,0 +1,39 @@
+#year_of_birth = 2002
+#print(id(year_of_birth))
+#
+#year_of_birth = year_of_birth + 1
+#print(id(year_of_birth))
+
+#lst = [1,2,3]
+#print(id(lst))
+
+#lst.append(4)
+#print(id(lst))
+
+
+#year_of_birth = 2002
+#print(type(year_of_birth))
+
+#year_of_birth = "2002"
+#print(type(year_of_birth))
+
+#year_of_birth: list = [1,2,3]
+#print(type(year_of_birth))
+
+#year_of_birth = "2002"
+#print(type(year_of_birth))
+
+#year_of_birth.append(4)
+
+#number_one = 12
+#number_two = 6
+
+#print(divmod(number_one, number_two))
+
+import math
+#import random
+#import os
+#import sys
+
+number = int(input("Enter the digit: "))
+print(math.sqrt(number))

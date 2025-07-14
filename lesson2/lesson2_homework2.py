2070ed057bcd3e657bf7c64892a0accb6f1a1126
dvyakymenko <dvyakymenko@gmail.com>
2025-07-14 13:34:53 +0300

    fourth commit

--- lesson2/lesson2_homework2.py
+++ lesson2/lesson2_homework2.py
@@ -1,3 +1,6 @@
 number = int(input("Введіть число з 5-ти цифр: "))
 
-print(number % 10,number % 100 // 10, number % 1000 // 100, number % 10000 // 1000, number % 100000 // 10000, sep="")
+print(number % 10,number % 100 // 10,
+      number % 1000 // 100,
+      number % 10000 // 1000,
+      number % 100000 // 10000, sep="")

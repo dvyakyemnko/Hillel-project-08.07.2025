d22428bbb274ff52c6198eae33e2eacc87941bd8
dvyakymenko <dvyakymenko@gmail.com>
2025-07-28 23:06:17 +0300

    7th commit

--- lesson4/lesson4_homework1.py
+++ lesson4/lesson4_homework1.py
@@ -1,9 +1,11 @@
-lst = [0, 5, 6, 0, 9, 55, 43, 19, 0, 0, 5, 3, 6, 18]
+lst = [0, 4, 5, 23, 77, 56, 2, 0 , 2, 6, 0, 3, 0, 67, 78]
 
+original_length = len(lst)
 non_zeros = [x for x in lst if x != 0]
+zero_count = original_length - len(non_zeros)
 
-zero_count = len(lst) - len(non_zeros)
+lst.clear()
+lst.extend(non_zeros)
+lst.extend([0] * zero_count)
 
-result = non_zeros + [0] * zero_count
-
-print(result)
+print(lst)

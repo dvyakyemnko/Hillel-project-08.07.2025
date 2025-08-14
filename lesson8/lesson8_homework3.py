a8be8bdb7803a6f4bb8a8339580e36757e39fa4d
dvyakymenko <dvyakymenko@gmail.com>
2025-08-14 12:07:36 +0300

    12th commit

--- lesson8/lesson8_homework3.py
+++ lesson8/lesson8_homework3.py
@@ -7,4 +7,4 @@ assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
 assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
 assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
 
-print("ОК")
+print("ОКі")
